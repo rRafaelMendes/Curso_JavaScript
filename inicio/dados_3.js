fa6aa4bcb486245a0bcd4590c3d1cea74a0570b4
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-02 11:51:55 -0300

    Create dados_3.js
    Tipos de Variáveis:
    #string
    #number
    boolean

---
+++ inicio/dados_3.js
@@ -0,0 +1,10 @@
+//Tipos de Variáveis 
+    // string
+let nome = "Renato";
+console.log(typeof nome);
+    //number
+let idade = 30;
+console.log(typeof idade);
+    //boolean
+let salarioRecebido = true // ou false
+console.log(typeof salarioRecebido); 
