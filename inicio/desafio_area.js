34489f0f9f82fca1abd7cf2b7c32351a490dd921
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-02 12:11:56 -0300

    Desafio para calcular a área da circunferência

---
+++ inicio/desafio_area.js
@@ -0,0 +1,8 @@
+const pi = 3.14;
+let raio = 10;
+let areaCirc = pi * (raio * raio);
+
+console.log("A area de um circulo de raio igual à " + raio + " é " + areaCirc + "m².");
+
+areaCirc = Math.PI * raio * raio
+console.log("A area de um circulo de raio igual à " + raio + " é " + areaCirc + "m².");
