0f906f2848ce031489c83004298418b94fe58ca5
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-02 10:56:18 -0300

    Finalizando sentenças e blocos de códigos

---
+++ inicio/blocos.js
@@ -0,0 +1,10 @@
+// Blocos de códigos sao criados a partir do simbolo {}
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
---
+++ inicio/desafio_sentenca_blocos.js
@@ -0,0 +1,18 @@
+/* Criar dois blocos de código, um com os números pares entre 1 e 10
+e o outro com os números ímpares entre 1 e 10*/
+{
+    console.log("Bloco #01");
+    console.log("1");
+    console.log("3");
+    console.log("5");
+    console.log("7");
+    console.log("9");
+}
+{
+    console.log("Bloco #02");
+    console.log("2");
+    console.log("4");
+    console.log("6");
+    console.log("8");
+    console.log("10");
+}
