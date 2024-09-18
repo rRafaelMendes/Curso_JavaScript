0cdf85ce23c18a7d8884bb4dfff774812e5aa81d
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-18 10:28:22 -0300

    Variáveis

---
+++ #02_fundamentos/variaveisEConstantes.js
@@ -0,0 +1,16 @@
+// A palavra reservada "var" caiu em desuso.
+
+var a = 3
+
+// A palavra reservada para criar variáveis "let" é mais usual.
+let b = 4
+
+//Podemos atribuir outros valores para variáveis.
+var a = 30
+b = 40
+console.log(a,b)
+
+const c = 5
+//uma constante não pode ser alterada seu valor
+c = 50 //vai gerar um erro: "TypeError: Assignment to constant variable."
+console.log(c)
