251af6da2b990e663de571f1952de5ade97b7bc4
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-18 10:18:47 -0300

    comentários

---
+++ #02_fundamentos/comentatio.js
@@ -0,0 +1,17 @@
+// Comentários de uma linha 
+console.log("linha 1")
+
+/*
+Comentário
+ de múltiplas 
+ linhas.
+*/
+console.log("linha 2")
+/*
+* comentário 
+* de múltiplas
+* linhas
+*
+*
+*/
+console.log("linha 3")
