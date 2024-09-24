e23717d68dedc3c288a3969d33d7a036b06f7d9e
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-24 17:08:53 -0300

    criando objetos a partir de função

---
+++ #02_fundamentos/objeto2.js
@@ -0,0 +1,13 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const Cliente = function() {}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {}
+console.log(typeof Produto)
+// função pode ser instanciada pela palavra new
+console.log(typeof new Produto())
+
+//A forma de criar objetos em JavaSript é  a partir de função.
