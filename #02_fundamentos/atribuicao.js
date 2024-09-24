3b5962aa5bcb7f4935be68deace4229332ef9146
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-24 19:40:38 -0300

    Atribuindo valores a uma variável

---
+++ #02_fundamentos/atribuicao.js
@@ -0,0 +1,17 @@
+const a =  5
+let b = 7
+
+b += a // o que seria b = b + a 
+console.log(b)
+
+b -= 5 // b = b - 5
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %= 2 // b = % 2
+console.log(b)
