13788c66ec7451e7e44c59a7235ee251850f0937
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-24 11:18:32 -0300

    Usando a variável let

---
+++ #02_fundamentos/usandoLet.js
@@ -0,0 +1,13 @@
+var numero = 12030
+{
+    let numero = 103045
+    console.log("dentro = ", numero)
+}
+console.log("Fora = ", numero)
+/*
+tem que ter de cabeça e isso variáveis definidas com a palavra reservada var tem escopo global e tem
+
+escopo de função e variáveis definidas como palavra reservada Let tem escopo global e escopo de função
+
+escopo de bloco.
+*/
