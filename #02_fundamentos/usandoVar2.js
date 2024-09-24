f47e2681b36b08ea21ccd5c1feb417126e15d4da
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-24 10:50:10 -0300

    Aula 02 Variáveis

---
+++ #02_fundamentos/usandoVar2.js
@@ -0,0 +1,7 @@
+var numero = 1
+{
+    var numero = 2
+    console.log("dentro = ", numero)
+}
+console.log("fora = ", numero)
+//O uso de variáveis globais pode causar erros no código. O exemplo mostra uma variável global definida e usada em outro bloco de código mudando a sua função e eficiência no código.
