a1a3133eef4e8b0a8195d2345dd21949e7c47610
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-25 11:35:46 -0300

    Operadores Aritméticos

---
+++ #02_fundamentos/aritmeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [4, 3, 2, 1]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplicacao)
+console.log(divisao)
+console.log(modulo)
