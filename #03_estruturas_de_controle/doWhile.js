635402079d89dd7e324ff42e3a7a7bbfd29745b8
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-27 09:46:55 -0300

    Estruturas de controle: doWhile

--- #03_estruturas_de_controle/doWhile.js
+++ #03_estruturas_de_controle/doWhile.js
@@ -2,10 +2,12 @@ function getInteiroAleatorioEntre(min, max) {
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
+
 let opcao = 0
 
-while(opcao != -1) {
+do {
     opcao = getInteiroAleatorioEntre(-1, 10)
-    console.log(`Opçao escolhida foi ${opcao}.`)
-}
+    console.log(`Opção escolhida foi ${opcao}.`)
+} while (opcao != -1);
+
 console.log("Até a próxima")
