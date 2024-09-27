0659183dc8305284b3b0217e0a8941563f56a57a
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-27 10:02:38 -0300

    Estruturas de controle: for

---
+++ #03_estruturas_de_controle/for1.js
@@ -0,0 +1,15 @@
+let contador = 1// declarar o contador
+
+while (contador <= 10)/*expressao*/ {
+    console.log(`contador = ${contador}`)
+    contador++ //incremento
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const nota = [9.34, 4.78, 2.63, 7.89]
+for ( let  i = 0; i < nota.length; i++ ) {
+    console.log(`nota = ${nota[i]}`)
+}
