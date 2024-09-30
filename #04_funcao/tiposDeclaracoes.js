5cafb47c03a0e40ecab7d5a221e7f5fa66afc558
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-09-30 11:21:56 -0300

    Funções: Tipos de declarações de funções.

---
+++ #04_funcao/tiposDeclaracoes.js
@@ -0,0 +1,19 @@
+//function declaration
+function soma(x, y) {
+    return x + y
+}
+console.log(soma(2, 3))
+
+
+//function expression
+const sub = function sub(x, y) {
+    return x - y
+}
+console.log(sub(19, 9))
+
+
+//named function expression
+const mult =  function mult(x, y) {
+    return x * y
+}
+console.log(mult(3, 5))
