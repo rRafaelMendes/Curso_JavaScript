7fbf8960df57b6407d76cb3751b390438d9ba0f9
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-10-08 18:52:21 -0300

    POO: principios importantes da POO

---
+++ #06_objeto/oo.js
@@ -0,0 +1,23 @@
+// CÓDIGO NAO EXECUTÁVEL 
+
+// Procedutal
+// Temos uma função que manuputla dados. Ex:
+processamento(valor1, valor2, valor3)
+
+//Orientada a Objeto
+// Temos um objeto que tem valores e funções 
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+objeto.processamento() // O foco passou a ser o objeto
+
+//Principios importantes:
+// 1. Abstração - trazer do mundo real para sua aplicação.
+// 2. Emcapsulamento - O Encapsulamento corresponde à uma proteção adicional aos dados de um objeto contra modificações impróprias.
+// 3. Herança (prototype) - Herança é um dos principais fundamentos em POO (Programação Orientada a Objeto), além de deixar o código mais limpo, a herança permite que uma classe herde de outra classe, todos os comportamentos e atributos. Através dela a classe possui imediatamente todas as funcionalidades de uma classe já existente.
+// 4. Polimorfismo - inversa da herança, onde a subclasse pode assumir as funcionalidades e características de sua superclasse. Imagine, por exemplo, as camisas de um time. Elas possuem formas e estilos diferentes da camisa de outro time, mas, apesar destas diferenças, elas têm a mesma funcionalidade, ou seja, são formas diferentes de camisas para representar o time nelas (cores, símbolos, etc.).
