a74587b5f57f8054b0fa04c4b2b3fcb931815b30
Renato Mendes <165093605+rRafaelMendes@users.noreply.github.com>
2024-10-08 17:04:25 -0300

    Criando uma função para resolver soma, multiplicação, divisão e subtração.

---
+++ #05_Lista_de_ecercícios/questao_01.js
@@ -0,0 +1,31 @@
+// Lista de Exercícios
+
+
+/*
+01) Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, subtração,
+multiplicação e divisão desses valores.
+*/
+
+//Soma
+function soma(a, b) {
+    return a + b
+}
+console.log(`Soma = ` + soma(3, 4))
+
+//Subtração
+function subtracao(a, b) {
+    return a - b
+}
+console.log(`Subtração = ` + subtracao(6, 4))
+
+//Divisão
+function divisao(a, b) {
+    return a / b
+}
+console.log(`Divisão = ` + divisao(6, 3))
+
+//Multiplicação
+function multiplicacao(a, b) {
+    return a * b
+}
+console.log(`Multiplicação = ` + multiplicacao(3, 5))
